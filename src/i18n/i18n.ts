3b44f976a411f42d28fb0c2c1472cb6d267e6948
Tyler Sturos <55340199+tjsturos@users.noreply.github.com>
2025-06-19 23:09:13 -0800

    add chinese to the config and add instructions

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,3 +1,11 @@
+// In order to add a new language, you need to:
+// 1. Add the language to the localeNames object in this file
+// 2. Run yarn lingui:extract on the command line, which creates the messages file in the i18n/<new-locale>/messages.po file
+// 3. Import the messages file here
+// 4. Add the language to the locales object in this file
+// 5. Run yarn lingui:compile on the command line, which creates the messages.js file in the i18n/<new-locale>/messages.ts file
+// 6. Commit the changes and push to the remote repository
+
 import { i18n, Messages } from "@lingui/core";
 
 // load language specific messages
@@ -5,7 +13,8 @@ import { messages as messagesEn } from "./en/messages";
 import { messages as messagesEs } from "./es/messages";
 import { messages as messagesFi } from "./fi/messages";
 import { messages as messagesRu } from "./ru/messages";
-// import { messages as messagesZh } from "./zh/messages";
+import { messages as messagesZhCN } from "./zh-CN/messages";
+import { messages as messagesZhTW } from "./zh-TW/messages";
 
 export interface Languages {
   [key: string]: string;
@@ -31,7 +40,8 @@ export const locales: LocaleMessages = {
   'es': messagesEs,
   'fi': messagesFi,
   'ru': messagesRu,
-  // 'zh': messagesZh,
+  'zh-CN': messagesZhCN,
+  'zh-TW': messagesZhTW,
 };
 
 /**
