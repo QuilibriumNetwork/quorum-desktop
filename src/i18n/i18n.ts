cea3fa8e67a71c1adccd552a4cf0f3e4874fa576
LaMat <65860122+lamat1111@users.noreply.github.com>
2025-07-01 09:52:21 +0200

    Compile tranlsations + Add comment to i18n.ts for translators

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,3 +1,20 @@
+// All the existing translations (apart from English) have been created using an LLM,
+// Communities are welcome to proofread and correct them.
+
+// To correct an exiisting language you need to:
+// 1. Correct the file src/i18n/<locale>/messages.po
+// 2. Run `yarn lingui:compile` on the command line, which updates the messages.js file in src/i18n/<locale>/messages.js
+// 3. Commit the changes and push to the remote repository
+
+// To add a new language, you need to:
+// 1. Add the language to the locales.ts file in src/118/locales.ts
+// 2. Run `yarn lingui:extract` on the command line, which creates the messages file in src/i18n/<new-locale>/messages.po
+// 3. Translate the messages in the messages.po file
+//    To translate via LLM, you can use https://github.com/lamat1111/po-files-translator
+// 4. Run `yarn lingui:compile` on the command line, which creates the messages.js file in src/i18n/<new-locale>/messages.js
+// 5. Commit the changes and push to the remote repository
+
+
 import { i18n } from "@lingui/core";
 import locales, { defaultLocale } from "./locales";
 
