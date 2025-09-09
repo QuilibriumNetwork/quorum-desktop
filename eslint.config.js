04d035f8388e706d39af2d28e52b2476e44d5f77
LaMat <65860122+lamat1111@users.noreply.github.com>
2025-09-09 12:42:40 +0200

    Update eslint ignored files - reorganize bug reports

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,21 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  { 
+    ignores: [
+      'dist',
+      'mobile/android/**',
+      'mobile/ios/**',
+      'node_modules/**',
+      '**/*.config.js',
+      'public/wasm_exec.js',
+      'mobile/babel.config.js',
+      'mobile/metro.config.js',
+      'mobile/postcss.config.js',
+      'mobile/tailwind.config.js',
+      'mobile/update-theme.js'
+    ] 
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
