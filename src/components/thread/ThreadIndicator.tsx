6822d77d4c6acc2c818b3d1685bcc6d067b95291
LaMat <65860122+lamat1111@users.noreply.github.com>
2026-03-09 14:57:30 +0100

    feat: add ThreadIndicator component for thread discovery on root messages

---
+++ src/components/thread/ThreadIndicator.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Icon } from '../primitives';
+import { useThreadStats } from '../../hooks/business/threads';
+import { formatMessageDate } from '../../utils/dateFormatting';
+import { t } from '@lingui/core/macro';
+import './ThreadIndicator.scss';
+
+interface ThreadIndicatorProps {
+  spaceId: string;
+  channelId: string;
+  threadId: string;
+  onClick: () => void;
+}
+
+export const ThreadIndicator: React.FC<ThreadIndicatorProps> = ({
+  spaceId,
+  channelId,
+  threadId,
+  onClick,
+}) => {
+  const { data: stats } = useThreadStats({ spaceId, channelId, threadId });
+  const replyCount = stats?.replyCount ?? 0;
+
+  return (
+    <Button
+      className="thread-indicator"
+      onClick={onClick}
+      variant="ghost"
+    >
+      <Icon name="messages" className="thread-indicator__icon" />
+      <span className="thread-indicator__count">
+        {replyCount === 0
+          ? t`View Thread`
+          : replyCount === 1
+            ? t`1 reply`
+            : t`${replyCount} replies`}
+      </span>
+      {stats?.lastReplyAt && (
+        <span className="thread-indicator__time">
+          {formatMessageDate(stats.lastReplyAt, true)}
+        </span>
+      )}
+    </Button>
+  );
+};
