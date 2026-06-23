762d39f2c23c8bea75b1277be6fc11a816235904
LaMat <65860122+lamat1111@users.noreply.github.com>
2026-06-23 17:21:44 +0200

    feat(notifications): add useGlobalNotifications composition hook

---
+++ src/hooks/business/notifications/index.ts
@@ -0,0 +1,7 @@
+export { useGlobalNotifications } from './useGlobalNotifications';
+export { GLOBAL_DISPLAY_CAP, GLOBAL_PER_CHANNEL_LIMIT } from './constants';
+export type {
+  GlobalNotification,
+  GlobalMentionNotification,
+  GlobalReplyNotification,
+} from './useGlobalNotifications';
